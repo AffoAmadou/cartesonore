200db47df6f01d37685f820d89fa1fa53cab31d9
christelle-bqn <christelle.boquillion@gmail.com>
2023-11-20 13:51:12 +0100

    add missing updated file

--- src/components/canvas/Examples.jsx
+++ src/components/canvas/Examples.jsx
@@ -3,21 +3,20 @@
 import { useGLTF, shaderMaterial } from '@react-three/drei'
 import { useFrame, useLoader, extend } from '@react-three/fiber'
 import * as THREE from 'three'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { PositionalAudio } from '@react-three/drei'
 import GSAP from 'gsap'
 
-import sound from "../../../public/sound/crow.mp3"
-
+import sound from '../../../public/sound/crow.mp3'
 
 import { Postcard } from './objects/Postcard'
 import { Sky } from './objects/Sky'
 import { Castle } from './objects/Castle'
 import { Intro } from './objects/Intro'
 import { Cloud } from './objects/Cloud'
+import { Kitchen } from './Kitchen'
 
 //Castle model
-
 
 //Raven model
 // export function RavenModel(props) {
@@ -26,8 +25,7 @@ import { Cloud } from './objects/Cloud'
 //   return <primitive object={scene} {...props} />
 // }
 
-
-// //!Raven 
+// //!Raven
 // export const Raven = () => {
 //   const soundref = useRef(null)
 
@@ -63,7 +61,7 @@ import { Cloud } from './objects/Cloud'
 //     void main() {
 //       vUv = uv;
 //       vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-//       gl_Position = projectionMatrix * viewMatrix * worldPosition;  
+//       gl_Position = projectionMatrix * viewMatrix * worldPosition;
 //     }
 //   `,
 //   // fragment shader
@@ -88,48 +86,60 @@ import { Cloud } from './objects/Cloud'
 // SkyShaderMaterial.depthWrite = false
 // extend({ SkyShaderMaterial });
 
-
 //!Scene Output scene
 export const Scene = () => {
   const meshref = useRef(null)
 
-  useFrame(({ clock }) => {
-  });
+  const [rotation, setRotation] = useState(true)
+  const [scene2D, setScene2D] = useState({ display: false, name: '' })
 
-  useEffect(() => {
+  useFrame(({ clock }) => {})
 
-  }, [])
-
-
+  useEffect(() => {}, [])
 
   return (
     <>
       <group position={[0, 0, 0]}>
-        <Postcard />
-        <Intro />
-        <Sky />
-        <Castle scale={.24} position={[0, -2, -3]} rotation={[0.0, 1.5, 0]} />
-        <Cloud image="1" position={[-2.6, -1.9, 0]} size={{ width: 4, height: 2 }} />
-        <Cloud image="2" position={[.1, -2.2, 0]} size={{ width: 3.3, height: 1.8 }} />
-        <Cloud image="0" position={[2, -1.6, 1]} size={{ width: 2.8, height: 1.6 }} />
+        {/* <Postcard /> */}
+        {/* <Intro /> */}
+        {/* <Sky /> */}
+
+        {scene2D.display && (
+          <mesh position={[1, 1, 0.5]} onClick={() => setScene2D({ name: 'kitchen', display: true })}>
+            <planeGeometry args={[1, 0.5, 64, 64]} />
+            <meshBasicMaterial attach='material' opacity={1} transparent color={0xffff00} />
+          </mesh>
+        )}
+
+        {scene2D.display && scene2D.name === 'kitchen' && <Kitchen />}
+
+        <Castle scale={0.24} position={[0, -2, -3]} rotation={[0.0, 1.5, 0]} />
+        {/* <Castle
+          scale={0.24}
+          position={[0, -2, -3]}
+          rotation={[0.0, 1.5, 0]}
+          setRotation={setRotation}
+          scene2D={scene2D}
+          setScene2D={setScene2D}
+        /> */}
+        <Cloud image='1' position={[-2.6, -1.9, 0]} size={{ width: 4, height: 2 }} />
+        <Cloud image='2' position={[0.1, -2.2, 0]} size={{ width: 3.3, height: 1.8 }} />
+        <Cloud image='0' position={[2, -1.6, 1]} size={{ width: 2.8, height: 1.6 }} />
 
         {/* middle*/}
-        <Cloud image="2" position={[-5, -2, -4]} size={{ width: 5.83, height: 3 }} />
-        <Cloud image="1" position={[0, -1.4, -4]} size={{ width: 6, height: 4 }} />
-        <Cloud image="0" position={[5.4, -2, -4]} size={{ width: 5.83, height: 3.8 }} />
-        <Cloud image="1" position={[2.6, -.65, -4.3]} size={{ width: 7, height: 4 }} />
-
+        <Cloud image='2' position={[-5, -2, -4]} size={{ width: 5.83, height: 3 }} />
+        <Cloud image='1' position={[0, -1.4, -4]} size={{ width: 6, height: 4 }} />
+        <Cloud image='0' position={[5.4, -2, -4]} size={{ width: 5.83, height: 3.8 }} />
+        <Cloud image='1' position={[2.6, -0.65, -4.3]} size={{ width: 7, height: 4 }} />
 
         {/* top */}
-        <Cloud image="0" position={[-5.5, -1, -6]} size={{ width: 7.3, height: 4.3 }} />
-        <Cloud image="2" position={[-.1, -.3, -5]} size={{ width: 8, height: 5 }} />
-
+        <Cloud image='0' position={[-5.5, -1, -6]} size={{ width: 7.3, height: 4.3 }} />
+        <Cloud image='2' position={[-0.1, -0.3, -5]} size={{ width: 8, height: 5 }} />
 
         {/* far*/}
-        <Cloud image="2" position={[9, .4, -13]} size={{ width: 8.83, height: 8 }} />
-        <Cloud image="1" position={[-7, .8, -9]} size={{ width: 5.83, height: 5 }} />
-
-      </group >
+        <Cloud image='2' position={[9, 0.4, -13]} size={{ width: 8.83, height: 8 }} />
+        <Cloud image='1' position={[-7, 0.8, -9]} size={{ width: 5.83, height: 5 }} />
+      </group>
     </>
   )
 }
