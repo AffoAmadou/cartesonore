89e40546bc573dc21b21c1f7faad8b86e6c64370
christelle-bqn <christelle.boquillion@gmail.com>
2023-11-22 13:31:09 +0100

    fix: scene2D.display undefined error in Examples.jsx

--- src/components/canvas/Examples.jsx
+++ src/components/canvas/Examples.jsx
@@ -31,20 +31,15 @@ export const Raven = () => {
 
   const playSound = () => {
     if (soundref.current) {
-      soundref.current.play();
+      soundref.current.play()
     }
-  };
+  }
   return (
     <RavenModel onClick={playSound} position={[-6, 2, -10]}>
-      <PositionalAudio
-        url={sound}
-        distance={1}
-        ref={soundref}
-      />
+      <PositionalAudio url={sound} distance={1} ref={soundref} />
     </RavenModel>
   )
 }
-
 
 //!Scene Output scene
 export const Scene = ({ isStarted, isPlaying, setIsStarted }) => {
@@ -59,39 +54,31 @@ export const Scene = ({ isStarted, isPlaying, setIsStarted }) => {
   const [isCastle, setIsCastle] = useState(false)
   const [isPostcard, setIsPostcard] = useState(true)
 
-
-
   const set = useThree((state) => state.set)
   const state = useThree((state) => state)
 
   let timeline = GSAP.timeline()
 
-
   return (
     <>
       <group position={[0, 0, 0]}>
-
-
         {!isStarted && <Intro />}
 
         <Lily position={[0, -1.4, 0]} />
         <Sky />
-        {
-          isPostcard && <Postcard isStarted={isStarted} isPlaying={isPlaying} setFirstClouds={setFirstClouds} setLastClouds={setLastClouds} setIsStarted={setIsStarted} setIsCastle={setIsCastle} setIsPostcard={setIsPostcard} />
-        }
-        {
-          scene2D.display && (
-            <mesh position={[1, 1, 0.5]} onClick={() => setScene2D({ name: 'kitchen', display: true })}>
-              <planeGeometry args={[1, 0.5, 64, 64]} />
-              <meshBasicMaterial attach='material' opacity={1} transparent color={0xffff00} />
-            </mesh>
-          )
-        }
+        {isPostcard && (
+          <Postcard
+            isStarted={isStarted}
+            isPlaying={isPlaying}
+            setFirstClouds={setFirstClouds}
+            setLastClouds={setLastClouds}
+            setIsStarted={setIsStarted}
+            setIsCastle={setIsCastle}
+            setIsPostcard={setIsPostcard}
+          />
+        )}
 
-
-        {
-          isCastle &&
-
+        {isCastle && (
           <Castle
             scale={0.24}
             position={[0, -2, -3]}
@@ -104,35 +91,28 @@ export const Scene = ({ isStarted, isPlaying, setIsStarted }) => {
             zoom={zoom}
             setZoom={setZoom}
           />
+        )}
 
-        }
-
-
-        {
-          scene2D === 'kitchen' && (
-            <Kitchen timeline={timeline} explore={scene2D} setScene2D={setScene2D} zoom={zoom} setZoom={setZoom} />
-          )
-        }
-
+        {scene2D === 'kitchen' && (
+          <Kitchen timeline={timeline} explore={scene2D} setScene2D={setScene2D} zoom={zoom} setZoom={setZoom} />
+        )}
 
         {/* //Apparition Nuages */}
-        {
-          firstClouds && <>
+        {firstClouds && (
+          <>
             <Cloud image='1' position={[-2.6, -2, 0]} size={{ width: 4, height: 2 }} />
             <Cloud image='2' position={[0.1, -2.3, 0]} size={{ width: 3.3, height: 1.8 }} />
             <Cloud image='0' position={[2, -1.6, 1]} size={{ width: 2.8, height: 1.6 }} />
           </>
-        }
+        )}
 
-        {
-          lastClouds &&
+        {lastClouds && (
           <>
             {/* middle*/}
-            < Cloud second={true} image='2' position={[-5, -2, -4]} size={{ width: 5.83, height: 3 }} />
+            <Cloud second={true} image='2' position={[-5, -2, -4]} size={{ width: 5.83, height: 3 }} />
             <Cloud second={true} image='1' position={[0, -1.4, -4]} size={{ width: 6, height: 4 }} />
             <Cloud second={true} image='0' position={[5.4, -2, -4]} size={{ width: 5.83, height: 3.8 }} />
             <Cloud second={true} image='1' position={[2.6, -0.65, -4.3]} size={{ width: 7, height: 4 }} />
-
 
             {/* top */}
             <Cloud second={true} image='0' position={[-5.5, -1, -6]} size={{ width: 7.3, height: 4.3 }} />
@@ -142,12 +122,10 @@ export const Scene = ({ isStarted, isPlaying, setIsStarted }) => {
             <Cloud second={true} image='2' position={[9, 0.4, -13]} size={{ width: 8.83, height: 8 }} />
             <Cloud second={true} image='1' position={[-7, 0.8, -9]} size={{ width: 5.83, height: 5 }} />
           </>
-        }
-
+        )}
 
         <Stats />
-
-      </group >
+      </group>
     </>
   )
 }
